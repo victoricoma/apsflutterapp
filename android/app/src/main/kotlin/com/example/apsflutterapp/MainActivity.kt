2ded0201708767a9a74e7bab42ef0170aa6850ce
Eberson <eberson.oliveira@gmail.com>
2025-05-13 20:46:33 -0300

    Ajuste de Build para Flutter

---
+++ android/app/src/main/kotlin/com/example/apsflutterapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.apsflutterapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
--- educacenter/android/app/src/main/kotlin/com/example/educacenter/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.educacenter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
